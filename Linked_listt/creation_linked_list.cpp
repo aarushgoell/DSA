1f1a180dbfeaaa7fde46ce854f19bd971faf03c7
Aarush Goel <aarushgoel2004@gmail.com>
2024-03-06 22:03:30 +0530

    My first commit

---
+++ Linked_listt/creation_linked_list.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node{
+    int data;
+    node *next;
+    node(int x){
+        data = x;
+        next = nullptr;
+    }
+}
+
+int main(){
+
+node *head = new node(10);
+node *temp1 = new node(20);
+node *temp2 = new node(30);
+head => next = temp1;
+temp1 => next = temp2;
+
+// below is an alternative of doing 
+// node *head = new node(10);
+// head => next = new node (20);
+// head => next => next = new node (30);
+
+    return 0;
+}
+
