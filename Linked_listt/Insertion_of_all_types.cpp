d20dfce3633f2d101361a2885728ca1f42db2f09
aarushgoell <aarushgoel2004@gmail.com>
2024-03-07 11:58:47 +0530

    next commit

---
+++ Linked_listt/Insertion_of_all_types.cpp
@@ -0,0 +1,64 @@
+// Below is Insertion at the beginning is constant time operation  O(1);
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node
+{
+    int data;
+    node *next;
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+
+};
+
+node *insertion(node *head, int x){
+    node *temp = new node(x);
+    temp->next = head;
+    return temp;
+}
+
+int main(){
+
+    node *head = NULL;
+    head = insertion(head,10);
+    head = insertion(head,20);
+    head = insertion(head,30);
+    cout<<head->data;
+    
+    return 0;
+}
+
+// Insertion at the end
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node
+{
+    int data;
+    node *next;
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+
+};
+
+node *insertion(node *head, int x){
+    node *temp = new node(x);
+    temp->next = head;
+    return temp;
+}
+
+int main(){
+
+    node *tail = NULL;
+    
+    cout<<head->data;
+    
+    return 0;
+}
--- Linked_listt/Traversing_a_linked_list.cpp
+++ Linked_listt/Traversing_a_linked_list.cpp
@@ -1,3 +1,34 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+    
+//    struct node
+//    {
+//         int data;
+//         node *next;
+//         node(int x){
+//             data = x;
+//             next = NULL;
+//         }
+//    };
+
+//    void printlist(node *head){
+//     node *curr = head;
+//     while(curr !=  NULL){
+//         cout<<curr->data<<" ";
+//         curr = curr->next;
+//     } 
+//    }
+   
+// int main(){
+//     node *head = new node(10);
+//     head->next = new node(20);
+//     head->next->next = new node(30);
+//     printlist(head);
+
+
+//     return 0;
+// }
+
 #include<bits/stdc++.h>
 using namespace std;
     
@@ -12,27 +43,22 @@ using namespace std;
    };
 
    void printlist(node *head){
-    node *curr = head;
-    while(curr !=  NULL){
-        cout<<curr->data<<" ";
-        curr = curr->next;
+    while(head !=  NULL){
+        cout<<head->data<<" ";
+        head = head->next;
     } 
    }
    
 int main(){
     node *head = new node(10);
-    head->next = new node(20);
-    head->next->next = new node(30);
+    printlist(head);
     printlist(head);
 
 
-
-
-
-
-
-   
-
     return 0;
 }
+
+
+
+
 
